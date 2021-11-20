3dd901c610bf7332a2b62f5cbf6b7b48dd5ae210
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-11-20 19:24:39 -0300

    add auth page

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getItem, setItem } from '../../hooks/useLocalStorage';
+import AuthFirebase from '../../services/auth';
 import './style.scss';
 
 export default function Auth() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const uid = getItem<string>("@todolist/uid")
+
+    if(uid) {
+      navigate(`/users/${uid}/todos`)
+    }
+  })
+  async function onClickHandler() {
+    const response = await AuthFirebase()
+        
+    setItem<string>("@todolist/uid", response)
+    navigate(`/users/${response}/todos`)
+  }
   return (
     <main>
-      <button className="auth-button">
+      <button className="auth-button" onClick={onClickHandler}>
         <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="none" viewBox="0 0 38 38">
           <path fill="#4285F4" d="M37.596 19.427c0-1.25-.11-2.438-.3-3.594H19.403v7.141h10.244c-.46 2.343-1.805 4.323-3.8 5.668v4.75h6.112c3.578-3.309 5.636-8.185 5.636-13.965z"/>
           <path fill="#34A853" d="M19.404 38c5.13 0 9.42-1.71 12.556-4.608l-6.112-4.75c-1.71 1.14-3.88 1.837-6.444 1.837-4.956 0-9.152-3.34-10.656-7.853H2.446v4.892C5.566 33.725 11.978 38 19.404 38z"/>
