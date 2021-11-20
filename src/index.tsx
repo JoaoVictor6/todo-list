7f42cc844941316af9a6e2a704583b282d5c87a0
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-11-20 13:08:21 -0300

    add react routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Auth from './pages/Auth';
 
 ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'),
 );
