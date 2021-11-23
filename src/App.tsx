14b77115f83c5f989ddd6290ee5cd48aed250d82
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-11-23 15:11:24 -0300

    apply firebase db

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,68 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router';
 import './App.scss';
 import Todo from './components/Todo';
+import { changeTodoData, deleteTodo, getTodos, writeUserTodo } from './services/database';
+
+function ID(): string {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 interface TodoProps {
   finished: boolean
   description: string
+  id: string
 }
 
 function App() {
   const [todos, setTodos] = useState<TodoProps[]>([]);
   const [inputDescription, setInputDescription] = useState('');
+  const { uid } = useParams()
+
+  useEffect(() => {
+    getTodos<TodoProps[]>(uid as string).then(data => {
+      if(data !== null){
+        data ? setTodos(data) : setTodos([])
+      }
+    })
+  }, [uid])
 
   function submitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
+    const idTodo = ID()
+    
     if (!inputDescription) return;
     setTodos((old) => [...old, {
       description: inputDescription,
       finished: false,
+      id: idTodo
     }]);
+
     setInputDescription('');
+    writeUserTodo(idTodo, inputDescription, false)
   }
 
   function changeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     const id = event.target.id;
-
     setTodos((oldTodos) => (
-      oldTodos.map((item:TodoProps, index) =>{
-        return id === index+'' ? {...item, finished: event.target.checked} : {...item};
+      oldTodos.map((item:TodoProps) =>{
+        if(id === item.id){
+          changeTodoData(item.id, item.description, event.target.checked)
+          return {...item, finished: event.target.checked}
+        } 
+        return {...item};
       })
     ));
+  
   }
 
   function deleteHandler(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     const id = event.currentTarget.id;
     setTodos((oldTodos) =>
-      [...oldTodos.filter((_, index) => index+'' !== id)],
+      [...oldTodos.filter((item) => item.id !== id)],
     );
+
+    deleteTodo(id)
   }
 
   return (
@@ -74,7 +100,7 @@ function App() {
               changeHandler={changeHandler}
               key={`toto_${index}`}
               description={item.description}
-              id={index + ''}/>
+              id={item.id}/>
           );
         })}
       </div>
